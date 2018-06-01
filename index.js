f797e015c9d7792628ea0b84e8e86609615a8036
Vimal Atreya Ramaka <vimal@ramaka.com>
2018-06-01 12:31:20 -0400

    clean up

--- index.js
+++ index.js
@@ -1,3 +1,2 @@
 require("@babel/register");
 module.exports.SingleAudioPlayer = require('./src/SingleAudioPlayer').default;
-// export { default as SingleAudioPlayer } from './src/SingleAudioPlayer';
