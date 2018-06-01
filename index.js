611ea60759208149a31cffddeac852bf3d93bcf4
Vimal Atreya Ramaka <vimal@ramaka.com>
2018-06-01 12:09:42 -0400

    use babel 7

---
+++ index.js
@@ -0,0 +1,3 @@
+require("@babel/register");
+module.exports.SingleAudioPlayer = require('./src/SingleAudioPlayer').default;
+// export { default as SingleAudioPlayer } from './src/SingleAudioPlayer';
--- src/index.js
+++
@@ -1,1 +0,0 @@
-export { default as SingleAudioPlayer } from './SingleAudioPlayer';
